ba8a34fc8ef36f6e2820ebcdcd2a38e12a2bf00d
Bhargav Bhavani <106801153+Bhargav5365@users.noreply.github.com>
2022-06-08 13:45:46 +0530

    Add files via upload
    variable and Arithmetic operators

---
+++ Dart/practice.dart
@@ -0,0 +1,24 @@
+void main() {
+  // variable
+
+  // var name = "Bhargav";
+  // var f_name = "raj";
+  //
+  // print(name);
+  // print(f_name);
+  // print(name.length);
+  // print(name.toUpperCase());
+  // print(name.hashCode);
+  // print(name.isEmpty);
+  // print(name.isNotEmpty);
+  // print(name.runtimeType);
+
+  // Arithmetic operators
+
+  // var a = 10;
+  // var b = 20;
+  // print(a + b);
+  // print(a - b);
+  // print(a / b);
+  // print(a * b);
+}
