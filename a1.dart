1177be0bb81e1a2a6e89654936e0d5c92e36181d
Bhargav <bhargav.bhavani@odaninfotech.com>
2022-06-08 16:10:41 +0530

    Constructor

---
+++ a1.dart
@@ -0,0 +1,49 @@
+// # Constructor
+/*
+Class_name(parameter){
+  constructor body
+}
+
+// Named Constructor
+Class_name.constructor_name(parameter){
+
+}
+
+*/
+
+// Dart Program to create a constructor
+
+// Creating Class named Gfg
+
+// void main() {
+//   // Creating an object
+//   Student std = new Student("Jones", 26);
+// }
+//
+// class Student {
+//   // Declaring a construstor
+//   Student(String str, int age) {
+//     print("The name is: ${str}");
+//     print("The age is: ${age}");
+//   }
+// }
+
+// Named Constructors
+
+void main() {
+  // Creating an object
+  Student std1 = new Student(); // object for Default constructor
+  Student std2 = new Student.namedConst(
+      "Computer Science"); // object for parameterized constructor
+}
+
+class Student {
+  // Declaring a construstor
+  Student() {
+    print("The example of the named constructor");
+  }
+  // Second constructor
+  Student.namedConst(String branch) {
+    print("The branch is: ${branch}");
+  }
+}
