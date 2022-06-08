36c7c6f6b66277e834467f9e112dfa496419594a
Bhargav <bhargav.bhavani@odaninfotech.com>
2022-06-08 16:31:03 +0530

    Multi-Level Inheritance

--- a2.dart
+++ a2.dart
@@ -1,12 +1,36 @@
 // # Inheritance
 // ## Single Inheritance
+// void main() {
+//   var obj = Son();
+//   obj.getValue(1000);
+//   obj.disp();
+// }
+//
+// // Parent Class / Super Class
+// class Father {
+//   late int money;
+//   getValue(m) {
+//     money = m;
+//   }
+// }
+//
+// // Child Class / Sub Class
+// class Son extends Father {
+//   String car = "i 10";
+//   disp() {
+//     print(car);
+//     print(money);
+//   }
+// }
+
+// ## Multi-Level Inheritance
 void main() {
-  var obj = Son();
+  var obj = GrandSon();
   obj.getValue(1000);
   obj.disp();
 }
 
-// Parent Class / Super Class
+// Parent Class
 class Father {
   late int money;
   getValue(m) {
@@ -14,11 +38,21 @@ class Father {
   }
 }
 
-// Child Class / Sub Class
+// Child Class
 class Son extends Father {
   String car = "i 10";
+  int bank_balance = 5000;
+  totalMoney() {
+    return money + bank_balance;
+  }
+}
+
+// GrandChild Class
+class GrandSon extends Son {
+  String bike = "K6";
   disp() {
+    print(bike);
     print(car);
-    print(money);
+    print(totalMoney());
   }
 }
